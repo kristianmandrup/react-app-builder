2ba448ac684f185d83f8c2d5a56ff4ef6f29fd13
Kristian Mandrup <kmandrup@gmail.com>
2018-09-16 11:36:14 +0100

    more indexer tests

--- src/components/layout/relative/indexer.test.js
+++ src/components/layout/relative/indexer.test.js
@@ -1,39 +1,89 @@
-import {createIndexer} from './indexer'
-import {types} from './types'
+import {createIndexer, contains, createPlacer} from './indexer'
 
-describe('createIndexer', () => {
-  const items = {
-    map: {
-      height_in_cm: {},
-      favourite: {},
-      display_name: {}
+const items = {
+  map: {
+    height_in_cm: {},
+    favourite: {},
+    display_name: {}
+  },
+  list: ['height_in_cm', 'favourite', 'display_name']
+}
+
+const indexer = createIndexer({items, keys})
+
+const methods = ['first', 'last', 'before', 'after']
+
+const layout = {
+  empty: {
+    height_in_cm: {},
+    favourite: {},
+    display_name: {}
+  },
+  shuffling: {
+    height_in_cm: {
+      first: true
     },
-    list: ['height_in_cm', 'favourite', 'display_name']
-  }
-
-  const indexer = createIndexer({items, keys})
-
-  const methods = ['first', 'last', 'before', 'after']
-
-  const layout = {
-    empty: {
-      height_in_cm: {},
-      favourite: {},
-      display_name: {}
+    favourite: {
+      last: true
     },
-    shuffling: {
-      height_in_cm: {
-        first: true
-      },
-      favourite: {
-        last: true
-      },
-      display_name: {
-        before: 'height_in_cm'
-      }
+    display_name: {
+      before: 'height_in_cm'
     }
   }
+}
 
+const keys = {
+  first: ['display_name'],
+  last: ['height_in_cm']
+}
+
+const $items = {
+  first: items.list[2],
+  last: items.list[1]
+}
+
+describe('contains', () => {
+  test('true when match', () => {
+    const item = $items.first
+    const match = contains(keys.first, item)
+    expect(match).toBe(true)
+  })
+
+  test('false when no match', () => {
+    const item = $items.last
+    const match = contains(keys.first, item)
+    expect(match).toBe(false)
+  })
+
+  test('false when no keys', () => {
+    const item = $items.last
+    const match = contains([], item)
+    expect(match).toBe(false)
+  })
+})
+
+describe('createPlacer', () => {
+
+  describe('first', () => {
+    const placer = createPlacer(items, keys.first)
+    const indexedItem = placer.first($items.first)
+
+    test('positions index first', () => {
+      expect(indexedItem.index).toBe(-1)
+    })
+  })
+
+  describe('last', () => {
+    const placer = createPlacer(items, keys.last)
+    const indexedItem = placer.first($items.last)
+
+    test('positions index last', () => {
+      expect(indexedItem.index).toBe(999)
+    })
+  })
+})
+
+describe('createIndexer', () => {
   test('contains mover methods', () => {
     methods.map(fn => expect(typeof indexer[fn]).toBe('function'))
   })
